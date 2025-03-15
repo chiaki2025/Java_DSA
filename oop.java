c6271252652b251cd4f0994a971d641bf3c2d2ab
chiaki2025 <tinvu13092003@gmail.com>
2025-03-15 18:31:01 +0700

    Change something

--- oop.java
+++ oop.java
@@ -1,8 +1,12 @@
+import java.util.*;
 public class oop{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		for(int i = 0 ; i < 10; i++){
 			System.out.print(i);
 		}
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+		System.out.println(number);
 	}
 }
