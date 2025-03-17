834df5f63950e3d76374122877dd4514f5a15ae0
chiaki2025 <tinvu13092003@gmail.com>
2025-03-17 16:22:22 +0700

    Wind direction is changing

--- oop.java
+++ oop.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class oop{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("Watashi wa Baka desu ka?");
 		for(int i = 0 ; i < 10; i++){
 			System.out.print(i);
 		}
